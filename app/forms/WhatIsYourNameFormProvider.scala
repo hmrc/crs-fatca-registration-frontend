44beb8e03d89cc83f149d5bf8a6b0d205acddda6
AashuChauhanHmrc <aashu.chauhan@digital.hmrc.gov.uk>
2023-10-26 16:26:04 +0100

    formprovider changes

--- app/forms/WhatIsYourNameFormProvider.scala
+++ app/forms/WhatIsYourNameFormProvider.scala
@@ -17,20 +17,32 @@
 package forms
 
 import javax.inject.Inject
-
 import forms.mappings.Mappings
 import play.api.data.Form
 import play.api.data.Forms._
 import models.WhatIsYourName
+import utils.RegexConstants
 
-class WhatIsYourNameFormProvider @Inject() extends Mappings {
+class WhatIsYourNameFormProvider @Inject() extends Mappings with RegexConstants {
+
+  private val maxLength = 35
 
   def apply(): Form[WhatIsYourName] = Form(
     mapping(
-      "firstName" -> text("whatIsYourName.error.firstName.required")
-        .verifying(maxLength(35, "whatIsYourName.error.firstName.length")),
-      "lastName" -> text("whatIsYourName.error.lastName.required")
-        .verifying(maxLength(3535, "whatIsYourName.error.lastName.length"))
+      "firstName" -> validatedText(
+        "whatIsYourName.error.firstName.required",
+        "whatIsYourName.error.firstName.invalid",
+        "whatIsYourName.error.firstName.length",
+        individualNameRegex,
+        maxLength
+      ),
+      "lastName" -> validatedText(
+        "whatIsYourName.error.lastName.required",
+        "whatIsYourName.error.lastName.invalid",
+        "whatIsYourName.error.lastName.length",
+        individualNameRegex,
+        maxLength
+      )
     )(WhatIsYourName.apply)(WhatIsYourName.unapply)
   )
 
