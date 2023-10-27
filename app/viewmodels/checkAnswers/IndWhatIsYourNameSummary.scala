6c357f771052a68123cbe581c68d55a210c625e3
AashuChauhanHmrc <aashu.chauhan@digital.hmrc.gov.uk>
2023-10-27 16:11:51 +0100

    Refactor

--- app/viewmodels/checkAnswers/IndWhatIsYourNameSummary.scala
+++ app/viewmodels/checkAnswers/IndWhatIsYourNameSummary.scala
@@ -25,7 +25,7 @@ import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
 import viewmodels.govuk.summarylist._
 import viewmodels.implicits._
 
-object WhatIsYourNameSummary {
+object IndWhatIsYourNameSummary {
 
   def row(answers: UserAnswers)(implicit messages: Messages): Option[SummaryListRow] =
     answers.get(IndWhatIsYourNamePage).map {
