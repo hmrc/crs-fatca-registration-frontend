276ecb3a9ce04121379ced42548d7e812978a2c1
AashuChauhanHmrc <aashu.chauhan@digital.hmrc.gov.uk>
2023-11-09 15:09:56 +0000

    Added tests

--- test/controllers/individual/IndWhereDoYouLiveControllerSpec.scala
+++ test/controllers/individual/IndWhereDoYouLiveControllerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.individual
 
 import base.SpecBase
@@ -12,12 +28,13 @@ import pages.IndWhereDoYouLivePage
 import play.api.inject.bind
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
+import views.html.IndWhereDoYouLiveView
 
 import scala.concurrent.Future
 
 class IndWhereDoYouLiveControllerSpec extends SpecBase with MockitoSugar {
 
-  lazy val indWhereDoYouLiveRoute = routes.IndWhereDoYouLiveController.onPageLoad(NormalMode).url
+  lazy val indWhereDoYouLiveRoute = controllers.individual.routes.IndWhereDoYouLiveController.onPageLoad(NormalMode).url
 
   val formProvider = new IndWhereDoYouLiveFormProvider()
   val form         = formProvider()
