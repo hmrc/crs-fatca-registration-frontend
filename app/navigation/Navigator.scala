726426d61b8ec7de0f53d1ed621c82b7ca7e4168
AashuChauhanHmrc <aashu.chauhan@digital.hmrc.gov.uk>
2023-12-04 09:07:41 +0000

    test fix

--- app/navigation/Navigator.scala
+++ app/navigation/Navigator.scala
@@ -121,11 +121,11 @@ class Navigator @Inject() () extends Logging {
           controllers.individual.routes.IndContactEmailController.onPageLoad(NormalMode),
           controllers.individual.routes.IndUKAddressWithoutIdController.onPageLoad(NormalMode)
         )
-    
-    case IndSelectAddressPage    => _ => controllers.individual.routes.IndContactEmailController.onPageLoad(NormalMode)
-    case IndContactEmailPage     => _ => controllers.individual.routes.IndContactHavePhoneController.onPageLoad(NormalMode)
-    case IndContactPhonePage     => _ => controllers.routes.CheckYourAnswersController.onPageLoad
-    case IndDateOfBirthPage      => whatIsYourDateOfBirthRoutes(NormalMode)
+
+    case IndSelectAddressPage => _ => controllers.individual.routes.IndContactEmailController.onPageLoad(NormalMode)
+    case IndContactEmailPage  => _ => controllers.individual.routes.IndContactHavePhoneController.onPageLoad(NormalMode)
+    case IndContactPhonePage  => _ => controllers.routes.CheckYourAnswersController.onPageLoad
+    case IndDateOfBirthPage   => whatIsYourDateOfBirthRoutes(NormalMode)
     case IndContactHavePhonePage =>
       userAnswers =>
         yesNoPage(
