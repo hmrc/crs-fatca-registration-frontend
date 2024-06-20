4f3e3cf3fdf6974881cab24b0d11702fa82120f3
matthewharris1 <33547722+matthewharris1@users.noreply.github.com>
2024-06-20 13:46:42 +0100

    DAC6-3234 | Amended bootstrap version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.6.0"
+  private val bootstrapVersion = "9.0.0"
   private val hmrcMongoVersion = "2.1.0"
 
   val compile = Seq[ModuleID](
