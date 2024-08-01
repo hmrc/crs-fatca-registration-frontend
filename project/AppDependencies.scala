130e5581af39f029bd54dae81184052b13b57ae9
Carl Thompson <carl.thompson@opencastsoftware.com>
2024-08-01 08:30:16 +0100

    update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion = "9.0.0"
-  private val hmrcMongoVersion = "2.1.0"
+  private val hmrcMongoVersion = "2.2.0"
 
   val compile = Seq[ModuleID](
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "10.1.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "2.0.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "10.5.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.1.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"     % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"             % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "domain-play-30"                 % "10.0.0",
