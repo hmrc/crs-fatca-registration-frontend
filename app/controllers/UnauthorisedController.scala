67017c4eeb17cd14d85bf41e809396e57e1ecf4a
matthewharris1 <matthew.harris@digital.hmrc.gov.uk>
2025-01-06 12:36:33 +0000

    DAC6-3343 | Amended content on UnauthorisedView

--- app/controllers/UnauthorisedController.scala
+++ app/controllers/UnauthorisedController.scala
@@ -16,6 +16,8 @@
 
 package controllers
 
+import config.FrontendAppConfig
+
 import javax.inject.Inject
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
@@ -24,13 +26,14 @@ import views.html.UnauthorisedView
 
 class UnauthorisedController @Inject() (
   val controllerComponents: MessagesControllerComponents,
-  view: UnauthorisedView
+  view: UnauthorisedView,
+  frontendAppConfig: FrontendAppConfig
 ) extends FrontendBaseController
     with I18nSupport {
 
   def onPageLoad: Action[AnyContent] = Action {
     implicit request =>
-      Ok(view())
+      Ok(view(frontendAppConfig.emailEnquiries))
   }
 
 }
