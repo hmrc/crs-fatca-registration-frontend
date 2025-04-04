2ef02b4dd99965f023449a71888a50b87d74aa4d
Carl Thompson <carl.thompson@opencastsoftware.com>
2025-04-04 12:55:11 +0100

    more dependencies updated

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,11 +7,11 @@ object AppDependencies {
 
   val compile = Seq[ModuleID](
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "11.12.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.2.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.0.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "domain-play-30"                        % "10.0.0",
+    "uk.gov.hmrc"       %% "domain-play-30"                        % "11.0.0",
     "org.typelevel"     %% "cats-core"                             % "2.10.0",
     "uk.gov.hmrc"       %% "crypto-json-play-30"                   % "8.2.0"
   )
