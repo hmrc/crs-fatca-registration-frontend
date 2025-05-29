77adc55b87fa6e1b52e64ec0436b14fcd6e5372f
Carl Thompson <77161245+C-Thomp@users.noreply.github.com>
2025-05-29 13:26:25 +0100

    DAC6-3709 | showBackLink = false (#235)
    * DAC6-3709 | showBackLink = false
    
    * DAC6-3709 | correct typo and address prbot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.11.0"
+  private val bootstrapVersion = "9.12.0"
   private val hmrcMongoVersion = "2.6.0"
 
   val compile = Seq[ModuleID](
@@ -12,7 +12,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "domain-play-30"                        % "11.0.0",
-    "org.typelevel"     %% "cats-core"                             % "2.10.0",
+    "org.typelevel"     %% "cats-core"                             % "2.13.0",
     "uk.gov.hmrc"       %% "crypto-json-play-30"                   % "8.2.0"
   )
 
@@ -21,12 +21,11 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
     "org.scalatestplus"          %% "scalacheck-1-15"         % "3.2.11.0",
     "org.scalatestplus"          %% "mockito-3-4"             % "3.2.10.0",
-    "org.mockito"                %% "mockito-scala"           % "1.17.31",
-    "org.scalacheck"             %% "scalacheck"              % "1.18.0",
-    "org.pegdown"                 % "pegdown"                 % "1.6.0",
-    "org.jsoup"                   % "jsoup"                   % "1.17.2",
+    "org.mockito"                %% "mockito-scala"           % "1.17.45",
+    "org.scalacheck"             %% "scalacheck"              % "1.18.1",
+    "org.jsoup"                   % "jsoup"                   % "1.20.1",
     "wolfendale"                 %% "scalacheck-gen-regexp"   % "0.1.2",
-    "com.softwaremill.quicklens" %% "quicklens"               % "1.9.7"
+    "com.softwaremill.quicklens" %% "quicklens"               % "1.9.12"
   ).map(_ % Test)
 
   val itDependencies: Seq[ModuleID] = Seq(
