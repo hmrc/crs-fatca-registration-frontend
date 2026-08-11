540f2a90afe9be7048e96215ab7669ddb05f93ec
witchelljack <36855484+WitchellJack@users.noreply.github.com>
2026-08-11 16:09:45 +0100

    DAC6-4411 - reverted library versions to fix build

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,18 +2,18 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.19.0"
-  private val hmrcMongoVersion = "2.13.0"
+  private val bootstrapVersion = "9.18.0"
+  private val hmrcMongoVersion = "2.7.0"
 
   val compile = Seq[ModuleID](
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.32.1",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.8.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "domain-play-30"                        % "11.0.0",
     "org.typelevel"     %% "cats-core"                             % "2.13.0",
-    "uk.gov.hmrc"       %% "crypto-json-play-30"                   % "8.4.0"
+    "uk.gov.hmrc"       %% "crypto-json-play-30"                   % "8.2.0"
   )
 
   val test = Seq(
